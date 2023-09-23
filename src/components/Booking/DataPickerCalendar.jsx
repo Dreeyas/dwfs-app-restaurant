9c6492293d76f81457bfb3888bd382f99961de64
Dreeyas <a.navarretegarrido@gmail.com>
2023-09-23 09:47:42 -0300

    change in picker calendar using props

--- src/components/Booking/DataPickerCalendar.jsx
+++ src/components/Booking/DataPickerCalendar.jsx
@@ -1,13 +1,14 @@
+import React from 'react';
 
-const DatePickerCalendar = ({ value, onClick }) => (
+const DatePickerCalendar = (props) => (
   <div className="input-group">
     <input
       type="text"
-      value={value}
-      onClick={onClick}
+      value={props.value}
+      onClick={props.onClick}
       className="form-control"
       placeholder="Select date"
-      />
+    />
   </div>
 );
 
